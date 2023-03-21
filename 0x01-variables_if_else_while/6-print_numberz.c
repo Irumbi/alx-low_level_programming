00b61d01697b16da238672cbd18c01be97b6bac7
Irumbi <irumbij@gmail.com>
2023-03-20 18:47:19 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *description - 'print single digits numbers of base 10 from 0'
+ *Return: 0
+*/
+int main(void)
+	{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+	}
