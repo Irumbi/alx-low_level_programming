2d1b1c17f7389a827a1846b0cd1d0669fb1ed871
Irumbi <irumbij@gmail.com>
2023-03-20 19:17:19 -0700

    print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *main = entry point
- *description - 'prints  all single digits of base 10 starting from 0'
+ *main - Entry point
+ *Description - 'prints  all single digits of base 10 starting from 0'
  *Return: 0
 */
 int main(void)
