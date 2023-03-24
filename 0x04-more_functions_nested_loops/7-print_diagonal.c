4554c376c633584a00f69b971473a3c9d6991790
Irumbi <irumbij@gmail.com>
2023-03-23 19:53:42 -0700

    7-diagonals | cat -e

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *print_diagonal - prints a diagonal line
+ *@n: the number of times to print the character
+ *
+ *Return; void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
