acb878765d5e22cd151fd19681edcf45b6c7d5be
Irumbi <irumbij@gmail.com>
2023-03-23 20:03:28 -0700

    8-squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_square - prints a square of agiven size
+ *@size: soze of thr square to be printed
+ *
+ *Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+		_putchar('#');
+		}
+	}
+}
