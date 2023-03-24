ab860345c2c384714ad163122e9cf790759856ea
Irumbi <irumbij@gmail.com>
2023-03-23 19:36:23 -0700

    6-lines | cat -e

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_line - prints aline of underscores
+ *@n: number to print
+ *
+ *Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
