b09406a19be66282a91467c80f9a643da2f51255
Irumbi <irumbij@gmail.com>
2023-03-29 18:53:30 -0700

    function that concatenates two strings(B)

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_strncat = concatenate two strings
+ *@dest: destination string
+ *src: source string
+ *@n: Maximum number of bytes to use from src
+ *
+ *Return: pointer to resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0, i;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
