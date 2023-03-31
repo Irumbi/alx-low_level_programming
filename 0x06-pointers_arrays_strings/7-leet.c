77648142391199180fe7e0eb8ed64c36a6f1e2c3
Irumbi <irumbij@gmail.com>
2023-03-30 19:27:44 -0700

    function that encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *leet - encodes a string into 1337
+ *@str: input string to encode
+ *
+ *Return: pointer to encode string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+
+			str[i] = numbers[j];
+			break;
+
+			}
+		}
+	}
+	return (str);
+}
