1b423454558f9fff3886f90ff3834fe7f384fbc5
Irumbi <irumbij@gmail.com>
2023-04-04 19:37:49 -0700

    function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ int int_putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
-
+char *_strpbrk(char *s, char *accept);
 
 
 
