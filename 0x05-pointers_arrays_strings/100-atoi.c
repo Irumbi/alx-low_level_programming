3e0d40c4a5bbee54051d6c22121cc66c43ac71a2
Irumbi <irumbij@gmail.com>
2023-04-10 08:07:30 -0700

    convert string to integers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *_atoi - converts string to an integer
+ *
+ *@s: the value to convert
+ *
+ *Return: on success sign times result, otherwise o
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+
+	while (*s)
+	{
+		if (*s == '-')
+		{
+			sign = -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			result = result * 10 + (*s - '0');
+		}
+		else
+		{
+			break;
+		}
+		s++;
+	}
+	return (sign * result);
+}
