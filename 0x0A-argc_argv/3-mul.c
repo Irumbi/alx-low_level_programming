11a72d83a04d6d428b534ad603e6622ea704f88d
Irumbi <irumbij@gmail.com>
2023-04-15 15:11:25 -0700

    aprogram that prints two numbers using argc and argv[]

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *
+ *@argc: Argument count
+ *@argv: Array of pointers to arguments
+ *
+ *Return: 0 on success. 1 on error
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+	printf("%d\n", result);
+
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 int main(int argc, char *argv[]);
-
+int multiply(int num1, int num2);
 
 #endif
