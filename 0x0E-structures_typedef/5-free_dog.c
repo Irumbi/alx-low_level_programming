c396ee3eacbe37e54848b9e7de99ee390c28b0c1
John Irumbi <irumbij@gmail.com>
2023-07-17 19:48:48 -0700

    5-free_dog.c done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *free_dog - frees dog
+ *@d: pointer to struct dog
+ *
+ *Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
