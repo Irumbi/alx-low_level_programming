ed8c05cef715fb28fe253aec4aa0a753ed02ad62
John  Irumbi <irumbij@gmail.com>
2023-08-01 13:34:39 -0700

    Task 1-listint_len function is done

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listint_len - Returns the number of elements in a listint_t list.
+ * listint_len - The number of elements in a linked listint_t list.
  * @h: Pointer to the head of the list.
  * Return: The number of elements in the list.
  */
@@ -11,8 +11,8 @@ size_t listint_len(const listint_t *h)
 
 	while (h != NULL)
 	{
-	node_cnt++;
 	h = h->next;
+		node_cnt++;
 	}
 
 	return (node_cnt);
